25187a7e36a0cacedcae5de05f4417ba3eafd475
kaz9112 <samuel.k.naiborhu@gmail.com>
2023-09-29 19:16:28 +0700

    first upload

---
+++ pseudocode.js
@@ -0,0 +1,4 @@
+// use existing template for filling form
+// use all need to filled form before ok
+// once filled move the form to preview form after filled, then ask user whether filled form is rights
+// once ok, turn it into a sql database using fastapi/flask?
