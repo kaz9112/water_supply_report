af4fb04b9046fa77519ce050ec8db0fd8b3857df
kaz9112 <samuel.k.naiborhu@gmail.com>
2023-09-30 20:45:52 +0700

    edited html to adapted to actual needs

--- pseudocode.js
+++ pseudocode.js
@@ -1,3 +1,4 @@
+// create an account system first
 // use existing template for filling form
 // use all need to filled form before ok
 // once filled move the form to preview form after filled, then ask user whether filled form is rights
