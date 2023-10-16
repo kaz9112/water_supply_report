07076be37f8f783add88251d7e2bd42c49ab69a9
kaz9112 <samuel.k.naiborhu@gmail.com>
2023-10-16 21:17:37 +0700

    added function to append report form to water_report.csv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
+import pandas as pd
 
 app = FastAPI()
 app.mount(
@@ -14,13 +14,43 @@ templates = Jinja2Templates(directory="templates")
 
 # list_tod = [] # debuging
 
+# function to append form from html to csv
+def insert_csv(origins, destination, date, volume, water_fill_hours, water_fill_minutes, water_deliver_hours, water_deliver_minutes):
+    db_report = pd.read_csv('water_report.csv')
+
+    form_dict = {
+        'origins': [origins],
+        'destination': [destination], 
+        'date': [date], 
+        'volume': [volume],
+        'waterFillHours': [water_fill_hours], 
+        'waterFillMinutes': [water_fill_minutes],
+        'waterDeliverHours': [water_deliver_hours], 
+        'waterDeliverMinutes': [water_deliver_minutes]
+    }
+    form_df = pd.DataFrame.from_dict(form_dict)
+
+    final_df = pd.concat([db_report, form_df], ignore_index=True)
+
+    final_df.to_csv('water_report.csv', index=False)
+    
+    return
+
+
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/")
-async def add_form(request: Request, destination: str = Form(...), date: str = Form(...)):
-    print(destination)
-    print(date)
-    print(waterfilling)
+async def add_form(
+    request: Request, 
+    destination: str = Form(...), 
+    date: str = Form(...), 
+    volume: int = Form(...),
+    waterFillHours: int = Form(...), 
+    waterFillMinutes: int = Form(...), 
+    waterDeliverHours: int = Form(...), 
+    waterDeliverMinutes: int = Form(...)
+    ):
+    insert_csv('WTP', destination, date, volume, waterFillHours, waterFillMinutes, waterDeliverHours, waterDeliverMinutes)
     return templates.TemplateResponse("index.html", {"request": request})
--- pseudocode.js
+++ pseudocode.js
@@ -2,4 +2,11 @@
 // use existing template for filling form
 // use all need to filled form before ok
 // once filled move the form to preview form after filled, then ask user whether filled form is rights
-// once ok, turn it into a sql database using fastapi/flask?
+// once ok, turn it into a csv database using fastapi?
+    // read whether csv exists or not? if exist?
+        // read_csv to df    
+        // turn form to pandas
+            // form turn it into dictionary
+            // from dict convert to pandas
+            // concat
+        // rewrite csv
