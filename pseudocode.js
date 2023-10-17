6a5c3c73b03b8d5dba3a9ae1143c4a76116b80ef
kaz9112 <samuel.k.naiborhu@gmail.com>
2023-10-17 07:43:26 +0700

    added hours system explaination on html, simplified value on destination selection

--- pseudocode.js
+++ pseudocode.js
@@ -1,8 +1,8 @@
-// create an account system first
-// use existing template for filling form
-// use all need to filled form before ok
-// once filled move the form to preview form after filled, then ask user whether filled form is rights
-// once ok, turn it into a csv database using fastapi?
+
+
+// use existing template for filling form !!Done
+// use all need to filled form before ok !!Done
+// once ok, turn it into a csv database using fastapi? !!DONE
     // read whether csv exists or not? if exist?
         // read_csv to df    
         // turn form to pandas
@@ -10,3 +10,13 @@
             // from dict convert to pandas
             // concat
         // rewrite csv
+
+// explain hours system 0 !!DONE
+// INput destination simplified on csv !!DONE
+
+// !!TODO!!
+
+
+
+// once filled move the form to preview form after filled, then ask user whether filled form is right
+// create an account system first
